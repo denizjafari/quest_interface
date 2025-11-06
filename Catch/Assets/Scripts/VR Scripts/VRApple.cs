c77156dc916f521954185f1be4e359522d05df23
Cecil Cao <pokemonball34@gmail.com>
2025-11-06 01:36:29 -0500

    Falling Apple in VR World

--- Catch/Assets/Scripts/VR Scripts/VRApple.cs
+++ Catch/Assets/Scripts/VR Scripts/VRApple.cs
@@ -1,11 +1,10 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class VRApple : MonoBehaviour
 {
     // Configs
-    private float decayTime = 10f;
+    private float decayTime = 3f;
     private float existing_timer = 0f;
     private bool isHeld = false;
     private GameObject cursor;
@@ -18,12 +17,99 @@ public class VRApple : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCenter = transform.position;
+        //cursor = GameObject.Find("Cursor").gameObject;
+        basket = GameObject.Find("Basket").gameObject;
+
+        // Load game difficulty config
+        GameInitConfig config = Helper.LoadGameInitConfig();
+        decayTime = config.selfDestroyTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Update parameter based on controller input
+        decayTime = ControllerListener.Instance.selfDestroyTime;
+        existing_timer += Time.deltaTime;
+
+        // Forcibly destroy this object if it exists for too long
+        if (existing_timer > decayTime)
+        {
+            Destroy(gameObject);
+        }
+
+        // Follow the cursor if player grabbed the apple
+        if (isHeld)
+        {
+            transform.position = cursor.transform.position;
+
+            // Let the object fall into the basket.
+            if (IsTargetOverBasket())
+            {
+                isHeld = false;
+                GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            }
+        }
+    }
+
+
+    private void OnDisable()
+    {
+        if (ScoreManager.Instance != null) ScoreManager.Instance.AddMiss(); // Decrement user score.
+        Destroy(gameObject);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.name == "Cursor" && !IsTargetOverBasket())
+        {
+            // The player "grabs" the apple, and this object should follow the cursor.
+            isHeld = true;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
+            transform.position = collision.transform.position;
+            
+        }
+        else if (collision.collider.gameObject.name == "Basket")
+        {
+            ScoreManager.Instance.AddScore();
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Check and return a bool if this object's position is hovering over the basket.
+    /// </summary>
+    /// <returns>Boolean representing if this object is over a basket.</returns>
+    bool IsTargetOverBasket()
+    {
+        Collider collider = GetComponent<Collider>(), basketCollider = basket.GetComponent<Collider>();
+        float xCenter = collider.bounds.center.x, xMin = basketCollider.bounds.min.x, xMax = basketCollider.bounds.max.x;
+
+        // Add a bit of buffer space for it to be easier to drop the apple into the basket
+        return xCenter < xMax - 0.2 && xCenter > xMin + 0.2;
+    }
+
+
+    /// <summary>
+    /// Fades in the target, automatically starting the apple to be invisible.
+    /// </summary>
+    /// <param name="fadeDuration">The seconds to fade the target in.</param>
+    /// <returns></returns>
+    public IEnumerator FadeIn(float fadeDuration)
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        Color finalColor = renderer.material.color;
+        Color initalColor = new Color(finalColor.r, finalColor.g, finalColor.b, 0f);
+
+        float timeElapsed = 0f;
+        while (timeElapsed < fadeDuration)
+        {
+            timeElapsed += Time.deltaTime;
+            renderer.material.color = Color.Lerp(initalColor, finalColor, timeElapsed / fadeDuration);
+            yield return null;
+        }
     }
 }
--- Catch/Assets/Scripts/VR Scripts/VRAppleSpawner.cs
+++ Catch/Assets/Scripts/VR Scripts/VRAppleSpawner.cs
@@ -1,28 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 /// <summary>
 /// Spawns Falling Apples in a 3D VR environment.
 /// </summary>
 public class VRAppleSpawner : MonoBehaviour
 {
+    // Internal Variables
     private float scaling_factor;
     private bool boundOnly;
     private float ROMScale;
-    private int counter;    // Used to determine which side to spawn next apple.
+    private int counter;                // Used to determine which side to spawn next apple.
+    private float maxArmRotation;
+    private float minArmRotation;
+    private bool isSpawning = false;
 
     // Component References
-    private GameObject currentApple;      // To keep track of the existing apple.
-    private Vector3 nextTargetPosition;   // To store the position of the next box
+    private GameObject currentApple;                                // To keep track of the existing apple.
+    private Vector3 nextTargetPosition = new Vector3(0, 1, 0.5f);   // To store the position of the next box
 
     [Header("Configs")]
     [Tooltip("Interval in seconds between apple spawns.")]
-    [SerializeField] float spawnLag;
+    [SerializeField] float spawnLag = 3f;
 
     [Header("Prefabs")]
     [Tooltip("The object the player needs to catch.")]
-    [SerializeField] GameObject applePrefab;        // Initialize in editor
+    [SerializeField] GameObject applePrefab;                        // Initialize in editor
 
     // Start is called before the first frame update
     void Start()
@@ -32,12 +37,22 @@ public class VRAppleSpawner : MonoBehaviour
         scaling_factor = config.scaling_factor;
         boundOnly = config.boundOnly;
         ROMScale = config.ROMScale;
-        if (config.spawnLag != 0) spawnLag = config.spawnLag;           // TEMPORARY FIX. MUST UPDATE JSONS
-
+        spawnLag = config.spawnLag != 0 ? config.spawnLag : spawnLag;       // TEMPORARY FIX. MUST UPDATE JSONS
 
         // TODO: Determine a way to limit rotation.
+        // Load the player's max/min arm rotation and screen boundaries.
+        ROMData rotationConfig = Helper.LoadShoulderRotationROM();
+        if (rotationConfig != null)
+        {
+            maxArmRotation = rotationConfig.max / 90f * Camera.main.orthographicSize * Camera.main.aspect * ROMScale;
+            minArmRotation = rotationConfig.min / 90f * Camera.main.orthographicSize * Camera.main.aspect * ROMScale;
+        }
+        else
+        {
+            maxArmRotation = 1f * Camera.main.orthographicSize * Camera.main.aspect * ROMScale;
+            minArmRotation = -1f * Camera.main.orthographicSize * Camera.main.aspect * ROMScale;
+        }
 
-        
         nextTargetPosition = ComputeNextTargetPosition(); // Pre-compute the initial position for the first box
     }
 
@@ -47,33 +62,71 @@ public class VRAppleSpawner : MonoBehaviour
         // Update parameter based on controller input
         scaling_factor = ControllerListener.Instance.scaling_factor;
 
-        //// Prepare the next spawn location
-        //if (currentBox == null && !isSpawning)
-        //{
-        //    isSpawning = true;
-        //    StartCoroutine(DelaySpawnRoutine(nextTargetPosition));
-        //    //currentBox = SpawnBox(nextTargetPosition);
-        //    nextTargetPosition = ComputeNextTargetPosition();
-        //}
+        // Prepare the next spawn location
+        if (currentApple == null && !isSpawning)
+        {
+            isSpawning = true;
+            //StartCoroutine(DelaySpawnRoutine(nextTargetPosition));
+            nextTargetPosition = ComputeNextTargetPosition();
+            currentApple = Instantiate(applePrefab, nextTargetPosition, Quaternion.identity);
+            isSpawning = false;
+        }
     }
 
-    void OnEnable()
-    {
-        
-    }
-
-    void OnDisable()
-    {
-        
-    }
-
+    
+    // TODO: Create a new spawning algorithm.
     /// <summary>
-    ///     Compute the next spawn point.
+    /// Compute the next spawn point.
     /// </summary>
     Vector3 ComputeNextTargetPosition()
     {
-        Vector2 screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        //Vector2 screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        float x;
 
-        return new Vector3(screenBounds.x, screenBounds.y, 0.5f);
+        // TODO: Implement Bound Only logic after min/max arm rotation is properly established.
+        //if (boundOnly)
+        //{
+        //    counter++;
+
+        //    if (counter % 2 == 1)
+        //    {
+        //        x = -1f;
+        //    }
+        //    else
+        //    {
+        //        x = 1f;
+        //    }
+        //}
+
+        //counter++;
+
+        //if (counter % 2 == 1)
+        //{
+        //    x = -1f;
+        //}
+        //else
+        //{
+        //    x = 1f;
+        //}
+
+        return new Vector3(0f, 3f, 0.5f);
+    }
+
+
+    /// <summary>
+    /// Spawns the next apple, slowly fading it into existence, then has it fall.
+    /// </summary>
+    /// <param name="position">Position to spawn the next apple</param>
+    IEnumerator DelaySpawnRoutine(Vector3 position)
+    {
+        currentApple = Instantiate(applePrefab, position, Quaternion.identity);
+        currentApple.transform.localScale = new Vector3(0.4f * scaling_factor, 0.4f * scaling_factor, 1);
+        currentApple.GetComponent<Rigidbody>().useGravity = false;
+
+        StartCoroutine(currentApple.GetComponent<FallingTarget>().FadeIn(spawnLag));
+        yield return new WaitForSeconds(spawnLag);
+
+        currentApple.GetComponent<Rigidbody>().useGravity = true;
+        isSpawning = false;
     }
 }
